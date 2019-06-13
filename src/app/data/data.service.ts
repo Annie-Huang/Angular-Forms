ddd7f3d39de20f5ad4b17662123e3b095aea07d7
Annie Huang <annie.hn@gmail.com>
2019-06-13 21:21:48 +1000

    Get dropdown list through observable in service and pip it to async keyword

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -10,6 +10,10 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
+  getSubscriptionTypes(): Observable<string[]> {
+    return of(['Monthly', 'Annual', 'Lifetime']);
+  }
+
   postUserSettingsForm(userSettings: UserSettings): Observable<any> {
     // return of(userSettings);
 
