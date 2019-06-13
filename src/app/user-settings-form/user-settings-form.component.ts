d603fd9af0e0e33040873f3daeb60a9745e2c527
Annie Huang <annie.hn@gmail.com>
2019-06-13 19:06:05 +1000

    Create a method in service and call the method from component

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserSettings} from '../data/user-settings';
 import {NgForm, NgModel} from '@angular/forms';
+import {DataService} from '../data/data.service';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -27,7 +28,7 @@ export class UserSettingsFormComponent implements OnInit {
   // deep copy can be done through lodash's deep copy function
   userSettings: UserSettings = {...this.originalUserSettings};
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
@@ -38,5 +39,9 @@ export class UserSettingsFormComponent implements OnInit {
 
   onSubmit(form: NgForm) {
     console.log('in onSubmit:', form.valid);
+    this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+      result => console.log('success: ', result),
+      error => console.log('error: ', error)
+    );
   }
 }
